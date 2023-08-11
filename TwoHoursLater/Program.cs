c5b36380c82ba1f3771d0b4f51f7d1ce4f98e154
Kirill <me4iniy@gmail.com>
2023-08-11 16:35:06 +0500

    Добавьте файлы проекта.

---
+++ TwoHoursLater/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TwoHoursLater
+{
+    public static class Programm
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
